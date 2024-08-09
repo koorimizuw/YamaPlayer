f5fb7585acbb9d036f1e4d9c86a3ef84d283a3b3
Koori SW <koorimizuw@gmail.com>
2024-08-09 18:10:09 +0900

    add no screen controller and logo (provisional)

--- Editor/EditorBase.cs
+++ Editor/EditorBase.cs
@@ -9,11 +9,12 @@ namespace Yamadev.YamaStream.Script
     public abstract class EditorBase : Editor
     {
         public string Title;
+        static string _logoGuid = "45177375d4933bc469e82e59e57ce065";
+        static float _paddingTop = 16f;
 
         public override void OnInspectorGUI()
         {
-            EditorGUILayout.Space(16f);
-            EditorGUILayout.LabelField($"{Title}", Styles.Title);
+            DrawLogoAndVersion(_paddingTop);
             EditorGUILayout.Space(16f);
             DrawLanguageSelector();
         }
@@ -29,6 +30,24 @@ namespace Yamadev.YamaStream.Script
                 Localization.CurrentLanguage = Localization.AvailableLanguages[selected];
                 EditorGUILayout.Space();
             }
+        }
+
+        public void DrawLogoAndVersion(float paddingTop)
+        {
+            Texture2D logo = AssetDatabase.LoadAssetAtPath<Texture2D>(AssetDatabase.GUIDToAssetPath(_logoGuid));
+            if (logo == null) return;
+
+            Rect rect = new Rect { height = 60f };
+            rect.width = rect.height * logo.width / logo.height;
+            rect.x = (EditorGUIUtility.currentViewWidth - rect.width) / 2f;
+            rect.y = paddingTop;
+            GUI.DrawTexture(rect, logo);
+
+            GUIContent version = new GUIContent($"v{Utils.GetYamaPlayerPackageInfo().version}");
+            Vector2 size = Styles.Bold.CalcSize(version);
+            GUI.Label(new Rect(rect.xMax, rect.yMax - size.y, size.x, size.y), version, Styles.Bold);
+
+            EditorGUILayout.Space(paddingTop + rect.height);
         }
     }
 }
