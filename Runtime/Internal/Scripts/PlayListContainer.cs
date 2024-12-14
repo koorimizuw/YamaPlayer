fd1f8f1596f6f83d2838248e3f1b06c0d61e54d2
Koori SW <koorimizuw@gmail.com>
2024-12-14 17:24:29 +0900

    update: prefab

--- Runtime/Internal/Scripts/PlayListContainer.cs
+++ Runtime/Internal/Scripts/PlayListContainer.cs
@@ -1,17 +1,12 @@
 ﻿
-using System;
 using UnityEngine;
-using UnityEngine.UI;
-using VRC.SDKBase;
-using VRC.Udon;
-using UdonSharp;
+
 
 namespace Yamadev.YamaStream.Script
 {
     public class PlayListContainer : MonoBehaviour
     {
-        [SerializeField]
-        Transform targetContent;
+        [SerializeField] Transform targetContent;
 
         public Transform TargetContent => targetContent;
     }
