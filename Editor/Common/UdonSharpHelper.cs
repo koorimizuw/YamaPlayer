5cced50c40389dcd9a4115f356d52bc7d765d5ec
Koori SW <koorimizuw@gmail.com>
2024-12-24 04:52:59 +0900

    update: add log

--- Editor/Common/UdonSharpHelper.cs
+++ Editor/Common/UdonSharpHelper.cs
@@ -1,5 +1,6 @@
 using UdonSharp;
 using UnityEditor;
+using UnityEngine;
 
 namespace Yamadev.YamaStream.Editor
 {
@@ -8,14 +9,22 @@ namespace Yamadev.YamaStream.Editor
     {
         static UdonSharpHelper()
         {
+            FixUdonSharpCompileError();
+        }
+
+        public static void FixUdonSharpCompileError()
+        {
             string path = VersionManager.PackageInfo.assetPath;
             UdonSharpProgramAsset[] assets = UdonSharpProgramAsset.GetAllUdonSharpPrograms();
             foreach (UdonSharpProgramAsset asset in assets)
             {
                 if (AssetDatabase.GetAssetPath(asset).IndexOf(path) != 0) continue;
                 MonoScript script = asset.sourceCsScript;
-                if (script.GetClass() == null) 
+                if (script.GetClass() == null)
+                {
+                    Debug.Log($"Udon script {script.name} has no class, trying to fix...");
                     AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(script), ImportAssetOptions.ForceUpdate);
+                }
             }
         }
     }
