793bdc649d6b61a8336e0afca50fec81c7faf497
Koori SW <koorimizuw@gmail.com>
2024-12-25 01:29:06 +0900

    fix: package info

--- Editor/Common/UdonSharpHelper.cs
+++ Editor/Common/UdonSharpHelper.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UdonSharp;
 using UnityEditor;
 using UnityEngine;
@@ -9,23 +11,36 @@ namespace Yamadev.YamaStream.Editor
     {
         static UdonSharpHelper()
         {
-            FixUdonSharpCompileError();
+            EditorApplication.delayCall += FixUdonSharpCompileError;
         }
+
+        static readonly string _lastCheckVersionKey = "YamaPlayer_FixUdonCompileErrorLastVersion";
+        static readonly string _checkedKeyBase = "YamaPlayer_FixUdonCompileErrorChecked";
+
+        static readonly List<MonoScript> _checkTargets = UdonSharpProgramAsset.GetAllUdonSharpPrograms().Where(asset =>
+        {
+            string packagePath = VersionManager.PackageInfo.assetPath;
+            return AssetDatabase.GetAssetPath(asset).StartsWith(packagePath) && asset.sourceCsScript != null;
+        }).Select(asset => asset.sourceCsScript).ToList();
 
         public static void FixUdonSharpCompileError()
         {
-            string path = VersionManager.PackageInfo.assetPath;
-            UdonSharpProgramAsset[] assets = UdonSharpProgramAsset.GetAllUdonSharpPrograms();
-            foreach (UdonSharpProgramAsset asset in assets)
+            if (EditorApplication.isPlaying) return;
+            string lastCheckVersion = SessionState.GetString(_lastCheckVersionKey, string.Empty);
+            if (lastCheckVersion == VersionManager.Version) return;
+            foreach (MonoScript target in _checkTargets)
             {
-                if (AssetDatabase.GetAssetPath(asset).IndexOf(path) != 0) continue;
-                MonoScript script = asset.sourceCsScript;
-                if (script.GetClass() == null)
+                string targetPath = AssetDatabase.GetAssetPath(target);
+                string checkedKey = $"{_checkedKeyBase}_{VersionManager.Version}_{targetPath}";
+                if (SessionState.GetBool(checkedKey, false)) continue;
+                if (target.GetClass() == null)
                 {
-                    Debug.Log($"Udon script {script.name} has no class, trying to fix...");
-                    AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(script), ImportAssetOptions.ForceUpdate);
+                    Debug.Log($"Udon script {target.name} has no class, trying to fix...");
+                    AssetDatabase.ImportAsset(targetPath, ImportAssetOptions.ForceUpdate);
                 }
+                SessionState.SetBool(checkedKey, true);
             }
+            SessionState.SetString(_lastCheckVersionKey, VersionManager.Version);
         }
     }
 }
